cbbd95ce7172e7fb0dc78078a5dd16c475837bee
Renan Noronha <renan.noronha@eemovelagro.com.br>
2025-04-15 21:53:57 -0300

    database class

--- src/service/db.py
+++ src/service/db.py
@@ -1,2 +1,26 @@
-class DB():
-    pass
+from sqlalchemy import create_engine
+
+from os import environ
+
+
+class SingletonMeta(type):
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class DB(metaclass=SingletonMeta):
+
+    def __init__(self):
+        self.engine = create_engine(environ.get("DATABASE_URL"))
+
+    def connect(self):
+        """
+        Connect to the database
+        """
+        return self.engine.connect()
