196883a5bacf8e9ce31baf23c6cdd821bc622eab
Renan Noronha <renan.noronha@eemovelagro.com.br>
2025-04-15 21:54:39 -0300

    fix: return

--- src/core/awards.py
+++ src/core/awards.py
@@ -1,14 +1,28 @@
+from sqlalchemy import text
 from src.model.awards import AwardsModel
 
 from src.service.db import DB
 
 
-class ActionCore:
+class AwardsCore:
 
     def __init__(self, *args, **kwargs):
         self.db = DB()
         self.model = AwardsModel()
 
     def get_longest_fastest_consecutive_awards(self):
-        awards = self.db.fetch_to_dict(self.model.get_longest_fastest_consecutive_awards())
-        return 200, awards
+        response = {}
+        with self.db.connect() as conn:
+            cursor_result = conn.execute(text(self.model.get_longest_consecutive_awards()))
+            longest_consecutive_awards = cursor_result.fetchall()
+            response["max"] = []
+            for obj in longest_consecutive_awards:
+                response["max"].append(dict(producers=obj[0], year=obj[1]))
+
+            cursor_result = conn.execute(text(self.model.get_fastest_consecutive_awards()))
+            fastest_consecutive_awards = cursor_result.fetchall()
+            response["min"] = []
+            for obj in fastest_consecutive_awards:
+                response["min"].append(dict(producers=obj[0], year=obj[1]))
+
+        return response, 200
