b3b021ee262e4a166e071b9fd55bcd11a7a54383
Renan Noronha <renan.noronha@eemovelagro.com.br>
2025-04-27 15:25:28 -0300

    fix

--- test/conftest.py
+++ test/conftest.py
@@ -8,21 +8,8 @@ environ["CSV_DELIMITER"] = ";"
 from src.api import app
 
 
-class Middleware:
-
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        environ.update({"custom:provider_id": 1})
-        environ.update({"custom:_id": 1})
-        environ.update({"email": "marcos.agnes@eemovel.com"})
-        return self.app(environ, start_response)
-
-
 @pytest.fixture()
 def application():
-    app.wsgi_app = Middleware(app.wsgi_app)
     yield app
 
 
